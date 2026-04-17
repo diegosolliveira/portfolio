661a637e2866910b51fa1533c5c3c11645c5eec2
diegosolliveira <diegolvr14@gmail.com>
2026-04-17 20:57:21 -0300

    new changes

--- script.js
+++ script.js
@@ -2,17 +2,7 @@ function scrollToSection(id) {
     document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
 }
 
-const header = document.querySelector("header");
-
-header.addEventListener("mousemove", (e) => {
-    const rect = header.getBoundingClientRect();
-
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-
-    header.style.setProperty("--x", `${x}px`);
-    header.style.setProperty("--y", `${y}px`);
-});
+// JS para o carousel
 
 const container = document.getElementById("cardContainer");
 const nextBtn = document.querySelector(".bi-chevron-right");
@@ -22,19 +12,16 @@ let index = 0;
 
 nextBtn.addEventListener("click", () => {
     const totalCards = container.children.length;
-    const visibleCards = getVisibleCards();
-
-    if (index < totalCards - visibleCards) {
-        index++;
-        updateCarousel();
-    }
+    index = (index + 1) % totalCards;
+    updateCarousel();
+    updateDots();
 });
 
 prevBtn.addEventListener("click", () => {
-    if (index > 0) {
-        index--;
-        updateCarousel();
-    }
+    const totalCards = container.children.length;
+    index = (index - 1 + totalCards) % totalCards;
+    updateCarousel();
+    updateDots();
 });
 
 function getVisibleCards() {
@@ -57,6 +44,40 @@ function updateCarousel() {
 
     container.style.transform = `translateX(-${index * move}px)`;
 }
+
+// js dots
+
+const dotsContainer = document.getElementById("dots");
+
+function createDots() {
+    const totalCards = container.children.length;
+
+    for (let i = 0; i < totalCards; i++) {
+        const dot = document.createElement("span");
+
+        dot.addEventListener("click", () => {
+            index = i;
+            updateCarousel();
+            updateDots();
+        });
+
+        dotsContainer.appendChild(dot);
+    }
+}
+
+
+function updateDots() {
+    const dots = dotsContainer.children;
+
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].classList.toggle("active", i === index);
+    }
+}
+
+createDots();
+updateDots();
+
+// JS para Efeito de digitação
 
 const words = [
   "Product Designer. ",
@@ -104,6 +125,8 @@ links.forEach(link => {
   });
 });
 
+// js para o toggle do tema
+
 const toggle = document.getElementById("themeToggle");
 const iconWrapper = document.getElementById("iconWrapper");
 
@@ -141,3 +164,146 @@ toggle.addEventListener("click", () => {
     iconWrapper.innerHTML = moonIcon;
   }
 });
+
+//JS para o background
+
+const deg = (a) => Math.PI / 180 * a
+const rand = (v1, v2) => Math.floor(v1 + Math.random() * (v2 - v1))
+const opt = {
+  particles: window.width / 500 ? 1000 : 500,
+  noiseScale: 0.009,
+  angle: Math.PI / 180 * -90,
+  h1: rand(0, 360),
+  h2: rand(0, 360),
+  s1: rand(20, 90),
+  s2: rand(20, 90),
+  l1: rand(30, 80),
+  l2: rand(30, 80),
+  strokeWeight: 1.2,
+  tail: 82,
+}
+const Particles = []
+let time = 0
+document.body.addEventListener('click', () => {
+  opt.h1 = rand(0, 360)
+  opt.h2 = rand(0, 360)
+  opt.s1 = rand(20, 90)
+  opt.s2 = rand(20, 90)
+  opt.l1 = rand(30, 80)
+  opt.l2 = rand(30, 80)
+  opt.angle += deg(random(60, 60)) * (Math.random() > .5 ? 1 : -1)
+  
+  for (let p of Particles) {
+    p.randomize()
+  }
+})
+
+class Particle {
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+    this.lx = x
+    this.ly = y
+    this.vx = 0
+    this.vy = 0
+    this.ax = 0
+    this.ay = 0
+    this.hueSemen = Math.random()
+    this.hue = this.hueSemen > .5 ? 20 + opt.h1 : 20 + opt.h2
+    this.sat = this.hueSemen > .5 ? opt.s1 : opt.s2
+    this.light = this.hueSemen > .5 ? opt.l1 : opt.l2
+    this.maxSpeed = this.hueSemen > .5 ? 3 : 2
+  }
+  
+  randomize() {
+    this.hueSemen = Math.random()
+    this.hue = this.hueSemen > .5 ? 20 + opt.h1 : 20 + opt.h2
+    this.sat = this.hueSemen > .5 ? opt.s1 : opt.s2
+    this.light = this.hueSemen > .5 ? opt.l1 : opt.l2
+    this.maxSpeed = this.hueSemen > .5 ? 3 : 2
+  }
+  
+  update() {
+    this.follow()
+    
+    this.vx += this.ax
+    this.vy += this.ay
+    
+    var p = Math.sqrt(this.vx * this.vx + this.vy * this.vy)
+    var a = Math.atan2(this.vy, this.vx)
+    var m = Math.min(this.maxSpeed, p)
+    this.vx = Math.cos(a) * m
+    this.vy = Math.sin(a) * m
+    
+    this.x += this.vx
+    this.y += this.vy
+    this.ax = 0
+    this.ay = 0
+    
+    this.edges()
+  }
+  
+  follow() {
+    let angle = (noise(this.x * opt.noiseScale, this.y * opt.noiseScale, time * opt.noiseScale)) * Math.PI * 0.5 + opt.angle
+    
+    this.ax += Math.cos(angle)
+    this.ay += Math.sin(angle)
+    
+  }
+  
+  updatePrev() {
+    this.lx = this.x
+    this.ly = this.y
+  }
+  
+  edges() {
+    if (this.x < 0) {
+      this.x = width
+      this.updatePrev()
+    }
+    if (this.x > width) {
+      this.x = 0
+      this.updatePrev()
+    }
+    if (this.y < 0) {
+      this.y = height
+      this.updatePrev()
+    }
+    if (this.y > height) {
+      this.y = 0
+      this.updatePrev()
+    }
+  }
+  
+  render () {
+    stroke(`hsla(${this.hue}, ${this.sat}%, ${this.light}%, .5)`)
+    line(this.x, this.y, this.lx, this.ly)
+    this.updatePrev()
+  }
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+  for (let i = 0; i < opt.particles; i++) {
+    Particles.push(new Particle(Math.random() * width, Math.random() * height))
+  }
+  strokeWeight(opt.strokeWeight)
+}
+
+
+function draw() {
+  time++
+  background(0, 100 - opt.tail)
+  
+  for (let p of Particles) {
+    p.update()
+    p.render()
+  }
+}
+
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight)
+}
+
+
